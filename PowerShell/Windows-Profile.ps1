98c210c3bee161408293030df272bb88f724281f
Merill Fernando <merill@merill.net>
2023-04-26 20:57:00 +1000

    Added Windows profile

---
+++ PowerShell/Windows-Profile.ps1
@@ -0,0 +1,6 @@
+# If any changes are made here, update https://github.com/merill/devconfig/blob/main/PowerShell/
+oh-my-posh --init --shell pwsh --config "F:\code\devconfig\ohmyposh\blue-owl-mf.json" | Invoke-Expression | Out-Null
+if(-not(Get-Module -name Terminal-Icons)) { Import-Module -Name PSReadLine | Out-Null }
+if(-not(Get-Module -name PSReadLine)) { Import-Module -Name PSReadLine | Out-Null }
+Set-PSReadLineOption -PredictionSource History -PredictionViewStyle ListView | Out-Null
+$env:PATH += ":/opt/homebrew/bin:/usr/local/bin"
