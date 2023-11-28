45dee6ef15eec5f9c81873c70a01f780347fd682
Merill Fernando <meferna@microsoft.com>
2023-11-28 18:26:20 +1100

    Update macOS-Profile.ps1

--- PowerShell/macOS-Profile.ps1
+++ PowerShell/macOS-Profile.ps1
@@ -1,6 +1,6 @@
 # If any changes are made here, update https://github.com/merill/devconfig/blob/main/PowerShell/
 /opt/homebrew/bin/oh-my-posh --init --shell pwsh --config /Users/merill/Documents/GitHub/devconfig/ohmyposh/blue-owl-mf.json | Invoke-Expression | Out-Null
-if(-not(Get-Module -name Terminal-Icons)) { Import-Module -Name PSReadLine | Out-Null }
+if(-not(Get-Module -name Terminal-Icons)) { Import-Module -Name Terminal-Icons | Out-Null }
 if(-not(Get-Module -name PSReadLine)) { Import-Module -Name PSReadLine | Out-Null }
 Set-PSReadLineOption -PredictionSource History -PredictionViewStyle ListView | Out-Null
 $env:PATH += ":/opt/homebrew/bin:/usr/local/bin"
