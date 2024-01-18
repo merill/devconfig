24bd72392ed8e3179b27c13cc4aadfa812961f6b
Merill Fernando <meferna@microsoft.com>
2024-01-18 21:09:26 +1100

    Update Windows-Profile.ps1

--- PowerShell/Windows-Profile.ps1
+++ PowerShell/Windows-Profile.ps1
@@ -1,5 +1,5 @@
 # If any changes are made here, update https://github.com/merill/devconfig/blob/main/PowerShell/
 oh-my-posh --init --shell pwsh --config "F:\code\devconfig\ohmyposh\blue-owl-mf.json" | Invoke-Expression | Out-Null
-if(-not(Get-Module -name Terminal-Icons)) { Import-Module -Name PSReadLine | Out-Null }
+if(-not(Get-Module -name Terminal-Icons)) { Import-Module -Name Terminal-Icons | Out-Null }
 if(-not(Get-Module -name PSReadLine)) { Import-Module -Name PSReadLine | Out-Null }
 Set-PSReadLineOption -PredictionSource History -PredictionViewStyle ListView | Out-Null
